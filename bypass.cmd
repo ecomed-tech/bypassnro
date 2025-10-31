fb4df6e6c25641c8fc898367b6cb2e0cb992f3f8
ecomed-tech <deon@ecomed.co.za>
2025-10-31 12:44:04 +0200

    Update bypass.cmd

--- bypass.cmd
+++ bypass.cmd
@@ -1,2 +1,2 @@
-curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/ecomed-tech/bypassnro/refs/heads/main/unattend.xml
+curl -L -o C:\Windows\Panther\unattend.xml https://https://github.com/ecomed-tech/bypassnro/refs/heads/main/unattend.xml
 %WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
